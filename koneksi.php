6f761a5a4b6e387d70a5993fbc00ef18c53d6651
radiancealam <62821601+radiancealam@users.noreply.github.com>
2020-05-29 21:33:12 +0700

    script hosting diedit bagian result.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$username = "root";
-$password = "";
-$db = "gamemath";
+$username = "id13879548_alam";
+$password = "w?^)3>mtxy^ZsR9h";
+$db = "id13879548_gamemath";
 $conn = mysqli_connect($host, $username, $password, $db);
 ?>
--- result.php
+++ result.php
@@ -4,20 +4,18 @@ session_start();
 if (isset($_POST["submit"])) {
     if ($_POST["jawab"] == $_SESSION["hasil"]) {
         $_SESSION["skor"] += 10;
-        echo "Jawaban Anda benar";
     } else {
         $_SESSION["skor"] -= 2;
         $_SESSION["lives"] -= 1;
-        echo "Jawaban Anda salah";
     }
-}
+} 
 if ($_SESSION["lives"] > 0) {
     header("Location: index.php");
     exit;
-} elseif ($_SESSION["lives"] == 0) {
-    $nama = $_SESSION["nama"];
-    $skor = $_SESSION["skor"];
-    header("Location: gameover.php");
-    exit;
-}
+    } elseif ($_SESSION["lives"] == 0) {
+        $nama = $_SESSION["nama"];
+        $skor = $_SESSION["skor"];
+        header("Location: gameover.php");
+        exit;
+    }
 ?>
