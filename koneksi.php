483d2e61e1d3bbb16af6b0321d75331c38631a20
Radiance Alam <62821601+radiancealam@users.noreply.github.com>
2020-05-30 18:44:14 +0700

    ubah pada koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,8 @@
 <?php
+// koneksi pada localhost
 $host = "localhost";
-$username = "id13879548_alam";
-$password = "w?^)3>mtxy^ZsR9h";
-$db = "id13879548_gamemath";
+$username = "root";
+$password = "";
+$db = "gamemath";
 $conn = mysqli_connect($host, $username, $password, $db);
 ?>
